f0e7a1a1817cd78b75b988c69bdfc8f15aeea12a
Paramee Thongtep <zai36997@gmail.com>
2019-09-10 14:01:12 +0700

    resolve check email

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,8 @@ const {body} = require('express-validator')
 /* GET users listing. */
 router.post('/register',
 body('name').not().isEmpty().withMessage('Please input name'),
-body('email').not().isEmpty().withMessage('Please input email'),
-body('password').not().isEmpty().withMessage('Please input password'),
+body('email').not().isEmpty().withMessage('Please input email').isEmail().withMessage('Wrong format Email'),
+body('password').not().isEmpty().withMessage('Please input password').isLength({min: 3}).withMessage('Password is more then 3 charecter'),
  userController.register)
  
 
