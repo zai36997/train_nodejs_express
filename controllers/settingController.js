63856104b66f0a7ca65d3bc0081d78fb156c0dfc
Paramee Thongtep <zai36997@gmail.com>
2019-09-10 11:27:40 +0700

    add function find

--- controllers/settingController.js
+++ controllers/settingController.js
@@ -1,3 +1,18 @@
-exports.index = (req,res,next) => {
-res.send("OK")
+
+const Setting = require('../models/setting')
+exports.index = async (req,res,next) => {
+     const {name,age} = req.body
+     const setting = await Setting.find()
+     
+    
+    // let setting = new User
+    // setting.name = name
+    // setting.age = age
+   
+    // await setting.save()
+    // console.log(req.body)
+
+    res.status(200).json({
+        data: setting
+    })
 }
---
+++ models/setting.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const schema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    age:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    creted:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Setting = mongoose.model("Setting", schema);
+module.exports = Setting
